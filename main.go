7d215d41f4a4eefe86aa399f09b15bad8644112a
Kurnia D Win <kurnia.d.win@gmail.com>
2023-04-17 05:39:07 +0700

    init

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/win-t/go-hello2-without-gomod/pkg/hello"
+
+func main() {
+	hello.SayHello()
+}
---
+++ pkg/hello/hello.go
@@ -0,0 +1,7 @@
+package hello
+
+import "github.com/win-t/go-hello-without-gomod/pkg/hello"
+
+func SayHello() {
+	hello.SayHello()
+}
